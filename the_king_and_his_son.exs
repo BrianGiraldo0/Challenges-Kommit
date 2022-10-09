4ec77b9171fc7047ff76bfd6c16500eecabc6a9f
Brian Giraldo <briand.giraldop@uqvirtual.edu.co>
2022-10-09 10:52:08 -0500

    Funcion para crear matriz de ruta

--- the_king_and_his_son.exs
+++ the_king_and_his_son.exs
@@ -1,42 +1,52 @@
-defmodule Lectura do
+defmodule Challenge do
 
-  def leer_caso(ruta) do
-    case File.read(ruta) do
-      {:ok, texto} -> String.split(texto, "\r\n", [trim: true])
+  def read_case(path) do
+    case File.read(path) do
+      {:ok, text} -> String.split(text, "\r\n", [trim: true])
       {:error, _} -> IO.puts("No existe el archivo")
     end
   end
 
-  def imprimir_lista([])  do
+  def print_list([])  do
 
   end
 
-  def imprimir_lista(list) do
+  def print_list(list) do
     IO.puts(hd list)
-    imprimir_lista(tl list)
+    print_list(tl list)
   end
 
-  def crear_matriz([], matriz) do
-    _ = matriz
+  def create_matrix([], matrix) do
+    _ = matrix
   end
 
-  def crear_matriz(list, matriz) do
-    crear_matriz(tl(list),  Tuple.append( matriz , List.to_tuple(String.to_charlist(hd(list)))))
+  def create_matrix(list, matrix) do
+    create_matrix(tl(list),  Tuple.append( matrix, List.to_tuple(String.to_charlist(hd(list)))))
   end
 
-  def encontrar_principe(matriz, i, j) do
+  def find_prince(matrix, i, j) do
     cond do
-      elem(elem(matriz, i), j) == ?@ -> {i, j}
-      i == tuple_size(matriz)-1 && j == tuple_size(elem(matriz, 0))-1 -> IO.puts("No se encontró ningún principe")
-      j == tuple_size(elem(matriz, 0))-1 -> encontrar_principe(matriz, i+1, 0)
-      true -> encontrar_principe(matriz, i, j+1)
+      elem(elem(matrix, i), j) == ?@ -> {i, j}
+      i == tuple_size(matrix)-1 && j == tuple_size(elem(matrix, 0))-1 -> IO.puts("No se encontró ningún principe")
+      j == tuple_size(elem(matrix, 0))-1 -> find_prince(matrix, i+1, 0)
+      true -> find_prince(matrix, i, j+1)
+    end
+  end
+
+  def create_route_matrix(matrix, i, j) do
+    cond do
+      i == tuple_size(matrix)-1 && j == tuple_size(elem(matrix, 0))-1 -> put_elem(matrix, i, put_elem(elem(matrix, i), j, :unchecked))
+      j == tuple_size(elem(matrix, 0))-1 -> create_route_matrix(put_elem(matrix, i, put_elem(elem(matrix, i), j, :unchecked)), i+1, 0)
+      true -> create_route_matrix(put_elem(matrix, i, put_elem(elem(matrix, i), j, :unchecked)), i, j+1)
     end
   end
 
 end
 
-list = Lectura.leer_caso("C:/Users/BRIAN/Downloads/Kommit/Challenges-Kommit/caso1.txt")
-Lectura.imprimir_lista(list)
-matriz = Lectura.crear_matriz(list, {})
-{i, j} = Lectura.encontrar_principe(matriz, 0, 0)
+list = Challenge.read_case("C:/Users/BRIAN/Downloads/Kommit/Challenges-Kommit/casoprueba.txt")
+Challenge.print_list(list)
+matrix = Challenge.create_matrix(list, {})
+{i, j} = Challenge.find_prince(matrix, 0, 0)
 IO.puts("El principe se encuentra en la ubicacion: fila #{i} columna #{j}")
+route_matrix = Challenge.create_route_matrix(matrix, 0, 0)
+IO.puts(route_matrix)
